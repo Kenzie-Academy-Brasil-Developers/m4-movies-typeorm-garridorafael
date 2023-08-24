93f3c151fbf17334af90a70745f187ae2bea78e2
Rafael <rgarrido846@gmail.com>
2023-08-24 14:12:58 -0300

    feat: create movie interfaces

---
+++ src/Interfaces/index.ts
@@ -0,0 +1,16 @@
+import {
+  MovieCreate,
+  MovieRead,
+  MovieUpdate,
+  MoviesRepo,
+} from "./movie.interface";
+import { Pagination, PaginationParams } from "./pagination.interface";
+
+export {
+  MovieCreate,
+  MovieRead,
+  MovieUpdate,
+  MoviesRepo,
+  Pagination,
+  PaginationParams,
+};
